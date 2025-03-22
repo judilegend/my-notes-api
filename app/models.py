278787aa4d0c1710cf49d6a1f44fee9eaf60c457
judilegend <judilegend2@gmail.com>
2025-03-22 15:46:24 +0300

    defin model user api

--- app/models.py
+++ app/models.py
@@ -1,14 +1,13 @@
 from app import db
 
-class Etudiant(db.Model):
-    __tablename__ = 'etudiants'
-    id = db.Column(db.Integer, primary_key=True)
-    nom = db.Column(db.String(100), nullable=False)
-    note = db.Column(db.Float, nullable=False)
+#definir une type enum pour le user
+class UserRole(enum.Enum):
+    ADMIN = 'admin'
+    ENSEIGNANT:'enseignant'
+    ETUDIANT:'etudiant'
 
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'nom': self.nom,
-            'note': self.note
-        }
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=true)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    role = db.Column(db.Enum(UserRole), nullable=False)
